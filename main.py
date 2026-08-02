b39d30589a8e5a21455426766c22355d1fbd5d22
farshidas2701-cpu <farshidas2701@gmail.com>
2026-08-02 16:27:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "سامانه پایش و رادار"
+    page.title = "سامانه پایش"
     page.theme_mode = ft.ThemeMode.DARK
     page.rtl = True
-    page.padding = 12
-    
+    page.padding = 20
+
     def show_login():
         page.controls.clear()
         page.add(
@@ -13,12 +13,12 @@ def main(page: ft.Page):
                 ft.Text("🔐 ورود به سامانه", size=24, weight=ft.FontWeight.BOLD),
                 ft.TextField(label="نام کاربری", width=300),
                 ft.TextField(label="رمز عبور", password=True, width=300),
-                ft.ElevatedButton("ورود", width=300),
+                ft.ElevatedButton("ورود", width=300, bgcolor="green", color="white"),
                 ft.Text("نسخه ساده و تست‌شده", size=10, color="grey500")
             ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER)
         )
         page.update()
-    
+
     show_login()
 
 ft.app(target=main)
